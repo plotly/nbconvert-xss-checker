2455e14571425005bf0ac0cbbf67dce16b72817d
tarzzz <tarun.gaba7@gmail.com>
2017-09-28 15:02:25 -0400

    Better/safer method of extracting html data

--- plotlyhtmlexporter/preprocessors.py
+++ plotlyhtmlexporter/preprocessors.py
@@ -1,14 +1,29 @@
+import jinja2
+import json
+import uuid
+import os
+
 from nbconvert.preprocessors.sanitize import SanitizeHTML
-from traitlets import Set
+
+DEFAULT_CHART_WIDTH = 600
+DEFAULT_CHART_HEIGHT = 400
 
 
 class PlotlySanitizeHTML(SanitizeHTML):
+    template = 'plot.tpl'
 
     def __init__(self, **kw):
         super(PlotlySanitizeHTML, self).__init__(**kw)
+        self.template_file = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), 'templates', 'plot.tpl')
 
-        # Add Plotly key to safe_output_keys:
-        safe_keys = self.safe_output_keys.update(['text/vnd.plotly.v1+html'])
+        # Add Plotly JSON key to safe_output_keys:
+        safe_keys = self.safe_output_keys.update(['application/vnd.plotly.v1+json'])
+
+    def template(self):
+        path, filename = os.path.split(self.template_file)
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(path))
+        return env.get_template(filename)
 
     def sanitize_code_outputs(self, outputs):
         """
@@ -17,13 +32,23 @@ class PlotlySanitizeHTML(SanitizeHTML):
         """
         outputs = super(PlotlySanitizeHTML, self).sanitize_code_outputs(outputs)
 
-        # Make `text/vnd.plotly.v1+html` content (which is trusted)
-        # available as `text/html`.
+        context = {'unique_div_id': uuid.uuid4()}
+
+        # Parse `text/vnd.plotly.v1+json` content and make it available as 'text/html'
         for output in outputs:
             if output['output_type'] in ('stream', 'error'):
                 continue
+
             data = output.data
             for key in data:
-                if key == 'text/vnd.plotly.v1+html':
-                    data['text/html'] = data[key]
+                if key == 'application/vnd.plotly.v1+json':
+                    figure = data[key]
+
+                    context['figure'] = json.dumps(figure)
+                    context['width'] = figure.get(
+                        'layout', {}).get('width', DEFAULT_CHART_WIDTH)
+                    context['height'] = figure.get(
+                        'layout', {}).get('height', DEFAULT_CHART_HEIGHT)
+                    data['text/html'] = self.template().render(**context)
+
         return outputs
